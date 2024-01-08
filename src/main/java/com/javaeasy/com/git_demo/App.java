2072f15f5d7aa032ce3f4d62b140aa19ea831e51
sirapuramverizon <srikanth.sirapuram@gmail.com>
2024-01-08 06:34:52 +0530

    added git all code to local repo

---
+++ src/main/java/com/javaeasy/com/git_demo/App.java
@@ -0,0 +1,17 @@
+package com.javaeasy.com.git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public void addData() {
+    	System.out.println("git demo project");
+    } 
+}
