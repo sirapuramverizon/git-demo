57cd32b8d109cd8475f2ac6be72dd0aeee8f7557
sirapuramverizon <155928128+sirapuramverizon@users.noreply.github.com>
2024-01-08 07:04:30 +0530

    Update App.java
    Adding method to demo pull

--- src/main/java/com/javaeasy/com/git_demo/App.java
+++ src/main/java/com/javaeasy/com/git_demo/App.java
@@ -4,7 +4,9 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
-
+        public void printData(){
+		System.out.println("method addedd toprint the data");
+	}
 	public void addData(String data) {
 		System.out.println("git demo project");
 		if (data.equals(Constant.DATA_TYPE)) {
