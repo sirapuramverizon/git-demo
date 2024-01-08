38aaee57383756c796076643e326cc0b80f1c702
sirapuramverizon <srikanth.sirapuram@gmail.com>
2024-01-08 06:53:40 +0530

    added constant class

--- src/main/java/com/javaeasy/com/git_demo/App.java
+++ src/main/java/com/javaeasy/com/git_demo/App.java
@@ -1,22 +1,16 @@
 package com.javaeasy.com.git_demo;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-    
-    public void addData(String data) {
-    	System.out.println("git demo project");
-    	if(data.equals("test")) {
-    		System.out.println("inside if");
-    	}else {
-    		System.out.println("inside else");
-    	}
-    } 
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+	public void addData(String data) {
+		System.out.println("git demo project");
+		if (data.equals(Constant.DATA_TYPE)) {
+			System.out.println("inside if");
+		} else {
+			System.out.println("inside else");
+		}
+	}
 }
---
+++ src/main/java/com/javaeasy/com/git_demo/Constant.java
@@ -0,0 +1,5 @@
+package com.javaeasy.com.git_demo;
+
+public class Constant {
+public static String DATA_TYPE="test";
+}
